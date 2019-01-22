7697ff52db0cd07310ee4a0c20eff719dba84878
Mrinalini Sinha <ms6471@nyu.edu>
2019-01-21 23:25:45 -0800

    added styling to navbar, changed products to be art. Only showing add art if logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './user/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle = 'Acme Product Management';
+  pageTitle = 'Mrinsin';
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
