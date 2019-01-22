a6470aae71a774591342f2e1754e4d3fef302fd4
Mrinalini Sinha <ms6471@nyu.edu>
2019-01-21 22:41:56 -0800

    used APM start as foundation for portfolio

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+/* Feature Modules */
+import { ProductModule } from './products/product.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messages/message.module';
+
+// Routing Feature
+import { RouterModule } from '@angular/router';
+
+const appRoutes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    ProductModule,
+    UserModule,
+    MessageModule
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
