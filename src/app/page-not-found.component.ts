2491fd603a531397fe7cb9aa67c5ce00f67e0104
Mrinalini Sinha <ms6471@nyu.edu>
2019-01-21 23:12:12 -0800

    changed font, background color.asses a line

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   template: `
-    <h1>This is not the page you were looking for!</h1>
+    <h2>This is not the page you were looking for!</h2>
     `
 })
 export class PageNotFoundComponent { }
